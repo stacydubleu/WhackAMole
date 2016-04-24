4d38e119fbccf4a0977188898a1bee3e041702b0
melo3450 <melo3450@gmail.com>
2016-04-24 10:14:12 -0400

    almost working game

--- app/src/main/java/hack/whackamole/Tile.java
+++ app/src/main/java/hack/whackamole/Tile.java
@@ -9,12 +9,10 @@ import java.util.*;
 
 public class Tile extends ImageView {
 
+    private String candidate;
     private int xCoord;
     private int yCoord;
-    private int Candidate = 0; //0 if available empty tile, any other number otherwise
-    private int randomNum;
     private boolean Target = false;
-    private static Timer timer = new Timer();
 
     public Tile(Context context, int x, int y) {
         super(context);
@@ -23,32 +21,33 @@ public class Tile extends ImageView {
         setImageResource(R.drawable.tile);
     }
 
-    public void updateTile() {
-        Random rand = new Random();
-        randomNum = rand.nextInt(4);
-//        if (randomNum == 0) {
-//            setImageResource(R.drawable.tile);
-//            Target = false;
-//        } else {
-//            Target = true;
-            if (randomNum == 0) {
+    public void updateTile(String candidate) {
+
+        switch (candidate){
+            case "Bernie Sanders":
                 setImageResource(R.drawable.bernie_sanders100x100);
-            } else
-            if (randomNum == 1) {
-                setImageResource(R.drawable.donald_trump100x100);
-            } else
-            if (randomNum == 2) {
+                this.candidate = candidate;
+                break;
+            case "Hilary Clinton":
                 setImageResource(R.drawable.hilary_clinton100x100);
-            } else {
+                this.candidate = candidate;
+                break;
+            case "Donald Trump":
+                setImageResource(R.drawable.donald_trump100x100);
+                this.candidate = candidate;
+                break;
+            case "Ted Cruz":
                 setImageResource(R.drawable.ted_cruz100x100);
-//            }
+                this.candidate = candidate;
+                break;
         }
+
+
     }
 
     public void isHit(){
         setImageResource(R.drawable.tile);
         Target=false;
-        randomNum=0;
     }
 
     public int getxCoord(){
@@ -65,8 +64,8 @@ public class Tile extends ImageView {
         Target = b;
     }
 
-    public int getCandidate(){
-        return randomNum;
+    public String getCandidate(){
+        return candidate;
     }
 
 }
